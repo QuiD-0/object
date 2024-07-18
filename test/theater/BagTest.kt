1f4b5a46ff3b2e1d2247d66481ddf17dda032179
quid-0 <wodnd101@naver.com>
2024-07-18 16:02:22 +0900

    생성자 테스트 추가

--- test/theater/BagTest.kt
+++ test/theater/BagTest.kt
@@ -8,6 +8,20 @@ import java.util.concurrent.atomic.AtomicLong
 class BagTest {
 
     @Test
+    fun constructor() {
+        val bag = Bag(AtomicLong(100))
+
+        assertEquals(100, bag.amount.get())
+    }
+
+    @Test
+    fun constructorError() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Bag(AtomicLong(-1))
+        }
+    }
+
+    @Test
     fun hasInvitation() {
         val invitation = Invitation(LocalDateTime.now())
         val audience = Audience(Bag(AtomicLong(100), invitation))
@@ -28,5 +42,22 @@ class BagTest {
 
         assertEquals(100, audience.bag.pay(100))
         assertEquals(0, audience.bag.amount.get())
+    }
+
+    @Test
+    fun payWithInvitation() {
+        val invitation = Invitation(LocalDateTime.now().plusHours(1))
+        val audience = Audience(Bag(AtomicLong(100), invitation))
+
+        assertEquals(100, audience.bag.pay(100))
+    }
+
+    @Test
+    fun payNotEnough() {
+        val audience = Audience(Bag(AtomicLong(100)))
+
+        assertThrows(IllegalArgumentException::class.java) {
+            audience.bag.pay(101)
+        }
     }
 }
