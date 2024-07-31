b1f6aec16963e517448064df38041b7916c154b0
quid-0 <wodnd101@naver.com>
2024-07-31 15:24:44 +0900

    시네마 개발 전 디렉터리 정리

--- src/main/kotlin/com/quid/spring/MovieApplication.kt
+++ src/main/kotlin/com/quid/spring/MovieApplication.kt
@@ -1,11 +1,11 @@
-package com.quid.`object`
+package com.quid.spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ObjectApplication
+class MovieApplication
 
 fun main(args: Array<String>) {
-	runApplication<ObjectApplication>(*args)
+	runApplication<MovieApplication>(*args)
 }
