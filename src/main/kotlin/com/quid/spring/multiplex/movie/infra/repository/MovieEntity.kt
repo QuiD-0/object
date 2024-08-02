0a707f4e7af395f2c96c03c8a0c732b2c4d3e814
quid-0 <wodnd101@naver.com>
2024-08-02 18:00:33 +0900

    개발 h2 설정

--- src/main/kotlin/com/quid/spring/multiplex/movie/infra/repository/MovieEntity.kt
+++ src/main/kotlin/com/quid/spring/multiplex/movie/infra/repository/MovieEntity.kt
@@ -1,23 +1,28 @@
 package com.quid.spring.multiplex.movie.infra.repository
 
-import jakarta.persistence.ElementCollection
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
+@Table(name = "MOVIE")
 class MovieEntity(
     @Id
+    @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long?,
+    @Column(name = "TITLE")
     val title: String,
+    @Column(name = "DURATION")
     val duration: Int,
+    @Column(name = "GENRE")
     val genre: String,
+    @Column(name = "RATING")
     val rating: String,
+    @Column(name = "DIRECTOR")
     val director: String,
     @ElementCollection(targetClass = String::class)
+    @CollectionTable(name = "ACTORS", joinColumns = [JoinColumn(name = "MOVIE_ID")])
     val actors: List<String>,
+    @Column(name = "PLOT")
     val plot: String
 ) {
 }
